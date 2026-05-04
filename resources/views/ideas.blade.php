a6e8c133d91169a3507b89942fee55ad50d6ab21
pilleeit <pille.eit@gmail.com>
2026-05-04 16:36:06 +0300

    lesson db - 8

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -14,13 +14,13 @@
   </div>
 </form>
 
-@if(count($ideas))
+@if($ideas->count())
   <div class="mt-6 text-white">
     <h2 class="font-bold">Your Ideas</h2>
 
     <ul class="mt-2">
       @foreach($ideas as $idea)
-        <li class="text-sm">{{ $idea }}</li>
+        <li class="text-sm">{{ $idea->description }}</li>
       @endforeach
     </ul>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
+use App\Models\Idea;
+// use Illuminate\Support\Facades\DB; !!siin ei kasuta db fasaadi
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $ideas = session()->get('ideas', []);
+    // $ideas = session()->get('ideas', []);
 
     // dd($ideas);
+
+    // $ideas = DB::table('ideas')->get();
+
+    // return $ideas[0]->description;
+
+    // võta üks idee modeli kaudu?
+    // $idea = Idea::find(1);
+    // dd($idea); //tagastab kogu info
+    // return $idea; // tagastab jasoni objekti
+
+    // $ideas = Idea::where('state', 'pending')->get(); // kui tahta vaadata neid mis tegemata
+
+    // $ideas = Idea::all(); // kui tahad kõiki saada
+
+    $ideas = Idea::query()
+        ->when(request('state'), function ($query, $state) {
+            // dd($state);
+            $query->where('state', $state);
+        })
+        ->get();
 
     return view('ideas', [
         'ideas' => $ideas,
@@ -16,9 +38,14 @@ Route::post('/ideas', function () {
     // dd('Hello!');
     // dd(request()->all());
 
-    $idea = request('idea');
+    // $idea = request('idea');
 
-    session()->push('ideas', $idea);
+    // session()->push('ideas', $idea); ei kasuta sessiooni enam
+
+    Idea::create([
+        'description' => request('idea'),
+        'state' => 'pending',
+    ]);
 
     return redirect('/');
 });
@@ -28,5 +55,4 @@ Route::get('/delete-ideas', function () {
     session()->forget('ideas');
 
     return redirect('/');
-
 });
