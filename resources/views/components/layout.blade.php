8507b04335f5493c25794bfbbd9e6fdadd95cd5e
pilleeit <pille.eit@gmail.com>
2026-05-17 15:52:27 +0300

    lesson front asset - DD 1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,16 +3,19 @@
 ])
 
 <!DOCTYPE html>
-<html lang="en" data-theme="dark">
+<html lang="en" data-theme="dracula">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+{{-- <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-<link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" /> --}}
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
-<body >
+<body class="text-primary">
 
     <x-nav />
 
